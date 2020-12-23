cf665198dd9c0302006375059b81476f0a360da0
moldux-dartus <moldux.dartus+github@gmail.com>
2020-12-23 11:42:44 -0600

    Ingredients list now recognizes null amounts

--- api/app.js
+++ api/app.js
@@ -15,12 +15,16 @@ var drinks_seed = JSON.parse(fs.readFileSync('./seed_drinks.json'))
 var drinks = drinks_seed.drinks.map(drink => {
     var ingredients = []
     for (let i = 1; i < 15; i++) {
-        if (drink['strIngredient' + i]!==null)
-            ingredients.push(drink['strIngredient' + i])
+        if (drink['strIngredient' + i] !== null) {
+            let measurment = drink['strMeasure' + i]
+            if (measurment !== null) {
+                ingredients.push(drink['strMeasure' + i] + ' ' + drink['strIngredient' + i])
+            }else {ingredients.push(drink['strIngredient' + i])}
+        }
     }
     return {
         name: drink.strDrink,
-        instructions: (drink.strInstructions.substring(0,500)),
+        instructions: (drink.strInstructions.substring(0, 500)),
         picture: drink.strDrinkThumb,
         ingredients: ingredients
     }
@@ -29,8 +33,8 @@ var drinks = drinks_seed.drinks.map(drink => {
 app.get('/drinks', (req, res) => {
     res.send(drinks)
 })
-app.get('/random', (req,res)=>{
-    let index = Math.floor(Math.random()*drinks.length)
+app.get('/random', (req, res) => {
+    let index = Math.floor(Math.random() * drinks.length)
     res.send(drinks[index])
 })
 app.get('/problems', function (req, res) {
